5ac9ca66d7a25f2c7c573cb047a8d87b4279ce29
+Michael Kolb <+michael.w.kolb@gmail.com>
2021-11-09 09:36:31 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+
+
+
+//1   
+    function moveDodgerLeft() {
+        
+        const dodger = document.getElementById("dodger");
+        const leftNumbers = dodger.style.left.replace("px", "");
+        const left = parseInt(leftNumbers, 10);
+      
+        if (left > 0) {
+          dodger.style.left = `${left - 1}px`;
+        }
+
+      }
+
+    document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowLeft") {
+          moveDodgerLeft();
+        }
+      });
+
+
+//2
+    function moveDodgerRight() {
+        const dodger = document.getElementById("dodger");
+        const rightNumbers = dodger.style.left.replace("px", "");
+        const right = parseInt(rightNumbers, 10);
+
+        if(right < 380) {
+          dodger.style.left = `${right + 1}px`;
+        }
+      }
+
+    document.addEventListener("keydown", function (e) {
+        if (e.key === "ArrowRight") {
+            moveDodgerRight();
+        }
+    });
